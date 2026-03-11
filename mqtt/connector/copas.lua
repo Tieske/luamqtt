16d1983f8bbe4d62daa6b068e6e79b5843cb4270
Thijs Schreijer <thijs@thijsschreijer.nl>
2026-03-11 22:41:36 +0100

    chore(copas): update log-scope

--- mqtt/connector/copas.lua
+++ mqtt/connector/copas.lua
@@ -84,21 +84,21 @@ function connector:send(data)
 		local owner = lock.owner and copas.getthreadname(lock.owner) or "n.a."
 		return nil, "failed acquiring send_lock: " .. tostring(err) .. " current owner: " .. owner
 	end
-	log:debug("[mqtt.connector.copas] send_lock acquired by %s", copas.getthreadname())
+	log:debug("[LuaMQTT.connector.copas] send_lock acquired by %s", copas.getthreadname())
 	if time > 1 then
-		log:warn("[mqtt.connector.copas] send_lock wait time above 1 second: %s seconds", time)
+		log:warn("[LuaMQTT.connector.copas] send_lock wait time above 1 second: %s seconds", time)
 	end
 
 	local i = 1
 	while i < #data do
 		i, err = sock:send(data, i)
 		if not i then
-			log:debug("[mqtt.connector.copas] send_lock released after error by %s: %s", copas.getthreadname(), tostring(err))
+			log:debug("[LuaMQTT.connector.copas] send_lock released after error by %s: %s", copas.getthreadname(), tostring(err))
 			lock:release()
 			return false, err
 		end
 	end
-	log:debug("[mqtt.connector.copas] send_lock released after send by %s", copas.getthreadname())
+	log:debug("[LuaMQTT.connector.copas] send_lock released after send by %s", copas.getthreadname())
 	lock:release()
 	return true
 end
